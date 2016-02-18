5279f1789f6f44994af88c7c3cb594edc4d2cdfb
Samuel Green <green.sam1192@gmail.com>
2016-02-18 03:47:47 -0500

    reorder classes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
 from app import db
 from app import app
 
+class Candidate(db.Model):
+	__tablename__ = 'candidate'
+	id = db.Column(db.Integer, primary_key=True)
+	rnd = db.Column(db.String(64))
+	name = db.Column(db.String(64))
+	votes = db.relationship('Vote', backref='candidate', lazy='dynamic')
+
+	def __repr__(self):
+		return '<Candidate {0}>'.format(self.name)
+
 class Vote(db.Model):
 	__tablename__ = 'vote'
 	id = db.Column(db.Integer, primary_key=True)
@@ -12,12 +22,3 @@ class Vote(db.Model):
 		return '<Vote of {0} for {1}, cast by {2}'\
 				.format(self.vote, self.candidate, self.voter)
 
-class Candidate(db.Model):
-	__tablename__ = 'candidate'
-	id = db.Column(db.Integer, primary_key=True)
-	rnd = db.Column(db.String(64))
-	name = db.Column(db.String(64))
-	votes = db.relationship('Vote', backref='candidate', lazy='dynamic')
-
-	def __repr__(self):
-		return '<Candidate {0}>'.format(self.name)
