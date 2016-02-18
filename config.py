b5467a92d2332f968e67d0c15b2b463a0be35742
Samuel Green <green.sam1192@gmail.com>
2016-02-18 03:18:01 -0500

    add requirements

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'this_is_CS51'
-
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
@@ -14,11 +11,11 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 if os.environ.get('ADMIN_PW') is None:
-	ADMIN_PW = 'secretBagMan'
-else:
 	ADMIN_PW = os.environ['ADMIN_PW']
+else:
+	exit(1)
 
 if os.environ.get('BROTHER_PW') is None:
 	BROTHER_PW = 'r0lltide'
 else:
-	BROTHER_PW = os.environ['BROTHER_PW']
+	exit(1)
